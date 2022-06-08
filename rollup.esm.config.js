b307ce4da5b99cc0fd779f1c827ecd19a9c6045b
zhangshichun <zhangshichun_looper@aliyun.com>
2022-06-08 16:13:01 +0800

    feat: 区分打包时esm和umd的行为

---
+++ rollup.esm.config.js
@@ -0,0 +1,24 @@
+import babel from '@rollup/plugin-babel';
+import path from 'path';
+import nodeResolve from '@rollup/plugin-node-resolve'
+import json from '@rollup/plugin-json';
+import esbuild from 'rollup-plugin-esbuild'
+
+export default {
+  input: path.resolve(__dirname, './src/index.ts'),
+  output: [
+    {
+      dir: path.resolve(__dirname, 'dist/esm'),
+      format: 'esm',
+    }
+  ],
+  preserveModules: true,
+  plugins: [
+    esbuild({
+      target: 'es2018'
+    }),
+    nodeResolve(),
+    json(),
+  ],
+  external: ['gsap']
+};
