72bf0c45e52b4047d48d4eab08767ebc76d68f56
zhangshichun <zhangshichun_looper@aliyun.com>
2022-06-09 15:26:37 +0800

    feat: add vue3 example

---
+++ examples/vue3/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my-image" id="myImage"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import rain from '../../../'
+
+onMounted(() => {
+rain('myImage', 'http://pic.zhangshichun.top/pic/20220606-02.jpg')
+})
+
+</script>
+
+<style>
+.my-image {
+  width: 600px;
+  height: 500px;
+}
+</style>
